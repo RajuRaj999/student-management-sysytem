63b5a0d61fb4822dfb980a2b5f9aa2644abc7430
RajuRaj999 <rajupspk999@gmail.com>
2023-12-31 18:12:04 +0530

    first commit

---
+++ src/main/java/com/student/sms/StudentManagementSysytemApplication.java
@@ -0,0 +1,20 @@
+package com.student.sms;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSysytemApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSysytemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+}
